6082eabb9650906ea61f5b04746347d77aa05a1c
YalmanchiliTejas <tyalaman@purdue.edu>
2024-11-17 10:16:00 -0500

    First commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,110 @@
+import { Row, Col } from "antd";
+import { withTranslation, TFunction } from "react-i18next";
+import { SvgIcon } from "../../common/SvgIcon";
+import Container from "../../common/Container";
+
+import i18n from "i18next";
+import {
+  FooterSection,
+  Title,
+  NavLink,
+  Extra,
+  LogoContainer,
+  Para,
+  Large,
+  Chat,
+  Empty,
+  FooterContainer,
+  Language,
+  Label,
+  LanguageSwitch,
+  LanguageSwitchContainer,
+} from "./styles";
+
+interface SocialLinkProps {
+  href: string;
+  src: string;
+}
+
+const Footer = ({ t }: { t: TFunction }) => {
+  const handleChange = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
+  const SocialLink = ({ href, src }: SocialLinkProps) => {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        key={src}
+        aria-label={src}
+      >
+        <SvgIcon src={src} width="25px" height="25px" />
+      </a>
+    );
+  };
+
+  return (
+    <>
+      <FooterSection>
+        <Container>
+          <Row justify="space-between">
+            <Col lg={10} md={10} sm={12} xs={12}>
+              <Language>{t("Contact")}</Language>
+              <Large to="/">{t("Tell us everything")}</Large>
+              <Para>
+                {t(`Do you have any question? Feel free to reach out.`)}
+              </Para>
+              <a href="tyalaman03@gmail.com">
+                <Chat>{t(`Let's Chat`)}</Chat>
+              </a>
+            </Col>
+      
+           
+          </Row>
+          <Row justify="space-between">
+            <Col lg={10} md={10} sm={12} xs={12}>
+              <Empty />
+              <Language>{t("Address")}</Language>
+              <Para>430 W Wood St</Para>
+              <Para>West Lafayette</Para>
+              <Para>Indiana</Para>
+            </Col>
+            <Col lg={8} md={8} sm={12} xs={12}>
+              <Title>{t("Company")}</Title>
+              <Large to="/">{t("About")}</Large>
+              <Large to="/">{t("Blog")}</Large>
+              <Large to="/">{t("Press")}</Large>
+              <Large to="/">{t("Careers & Culture")}</Large>
+            </Col>
+           
+          </Row>
+        </Container>
+      </FooterSection>
+      <Extra>
+        <Container border={true}>
+          <Row
+            justify="space-between"
+            align="middle"
+            style={{ paddingTop: "3rem" }}
+          >
+            <NavLink to="/">
+              <LogoContainer>
+                <SvgIcon
+                  src="logo.svg"
+                  aria-label="homepage"
+                  width="101px"
+                  height="64px"
+                />
+              </LogoContainer>
+            </NavLink>
+          
+          </Row>
+        </Container>
+      </Extra>
+    </>
+  );
+};
+
+export default withTranslation()(Footer);
